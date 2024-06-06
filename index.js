71e1f67838c79c2731862e93779aa96500afc87f
Pokala57 <sathish@vridhee.com>
2024-06-06 21:56:21 +0530

    after

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('navbar-toggler').addEventListener('click', function() {
+    const nav = document.getElementById('navbarNavAltMarkup');
+    nav.classList.toggle('show');
+  });
+  

99186c096ebdb7f6c4bf84b6b2006b611cc0e7b5
Pokala57 <sathish@vridhee.com>
2024-06-12 20:52:42 +0530

    after js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-document.getElementById('navbar-toggler').addEventListener('click', function() {
-    const nav = document.getElementById('navbarNavAltMarkup');
-    nav.classList.toggle('show');
-  });
-  
+function switchOff() {
+
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png"
+
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png"
+
+    document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9"
+
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e"
+
+    document.getElementById("switchStatus").textContent = "Switched Off"
+
+}
+
+
+function switchOn(){
+    
+}
+
+
